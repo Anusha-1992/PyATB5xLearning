c350b75bfc72596646e5c75551a515d4aa85e6a3
Anusha-1992 <anushabk22@gmail.com>
2024-11-11 21:25:11 +0530

    Python_basic

---
+++ nov/ex_09nov2024/Lab022_Task2.py
@@ -0,0 +1,14 @@
+num1 = int(input("enter the num1"))
+num2 = int(input("enter the num2"))
+
+div =num1 / num2
+print("div of the two num: " , div)
+
+q = num1 // num2
+
+print(" questioned num : " , q)
+print(type(q))
+
+r = num1 % num2
+print("reminder of div : " , r)
+print(type(r))
